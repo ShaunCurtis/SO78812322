70c5974e8fff977a37b0f51cd8a1c31841719abb
Shaun Curtis <shaun@coldelm.com>
2024-07-31 08:37:41 +0100

    Add project files.

---
+++ SO78812322/Components/DisposableService.cs
@@ -0,0 +1,21 @@
+﻿public interface IDisposableService : IDisposable, IAsyncDisposable { }
+
+public class DisposableService : IDisposableService
+{
+    private static int globalId = 0;
+    private int id;
+    private readonly ILogger<DisposableService> logger;
+    private readonly string type;
+
+    public DisposableService(ILogger<DisposableService> logger, string type)
+    {
+        this.logger = logger;
+        this.type = type;
+        id = ++globalId;
+
+        logger.LogInformation("{id}: {type} Service Instance Created", type, id);
+    }
+
+    public void Dispose() => logger.LogWarning("{id}: {type} Service Disposed!", type, id);
+    public async ValueTask DisposeAsync() => logger.LogWarning("{id}: {type} Service Disposed Asynchronously!", type, id);
+}
